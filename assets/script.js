064748bfe105919f218b0242334ce80d4a4a59a5
Marco Jansen <marco.jansen@gmx.us>
2020-12-23 07:31:53 +0800

    added UVIndex URL request, added all values to HTML via Javascript

--- assets/script.js
+++ assets/script.js
@@ -2,117 +2,146 @@
 const apiKey = "a68c335056a4a3d1000ef6aa6b67cc0b";
 const searchedCities = [];
 let previousSearches;
-// var div = $("<div class='container'>");
+let indexUV;
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+// URL queries
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+let queryURL5Days = "https://api.openweathermap.org/data/2.5/forecast?q=";
+// let queryURLUVIndex = "https://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}";
 
 init();
 
-function init(){
-
+function init() {
+    let city = getLastSearch();
     // request last cities from localstorage
 
-    let previousSearch = localStorage.getItem("cities");
-    console.log(previousSearch);
+    if (city === "empty") {
+        // searchByGeolocation();
+    }
+    else {
+        clear();
+        getCurrentWeather(city);
+        getFiveDayForecast(city)
+    }
+    // let previousSearch = localStorage.getItem("cities");
+    // console.log(previousSearch);
 
     //call getweather for last item in local storage
-    
-    let lastIndexLocalStorage = previousSearch.length;
-    console.log(lastIndexLocalStorage);
+
+    // let lastIndexLocalStorage = previousSearch.length;
+    // console.log(lastIndexLocalStorage);
 
     // diaplay weather for the above inti
 }
 
+function clear() {
+    //all data in the weather 
+    $("#dateCurrent").empty();
+    $("#iconImage").empty();
+    $("#cityCurrent").empty();
+    $("#tempCurrent").empty();
+    $("#humidityCurrent").empty();
+    $("#windCurrent").empty();
+    $("#UVCurrent").empty();
+}
+
+function getLastSearch() {
+    let city = "empty";
+    // get values of local storage;
+    let searchedValues = JSON.parse(localStorage.getItem("cities"));
+
+    console.log(searchedValues.length);
+
+    if (searchedValues.length == null) {
+        city = "Perth";
+        return city;
+    }
+    else {        
+        city = searchedValues[searchedValues.length - 1];
+        return city;
+    }
+
+}
+
 $("#searchButton").on("click", searchWeather);
 
-function searchWeather(){
+function searchWeather() {
     var cityName = $("#citySearch").val().trim();
     console.log(cityName);
     getCurrentWeather(cityName); // temp, humidity, windspeed, uv
     getFiveDayForecast(cityName); // five days into future five cards
 }
 
-function getCurrentWeather(city){
-   var queryWeatherUrl = queryURL+city+"&appid="+apiKey;
-  
+// function to request and return UV Index
+function getUVIndex(lon,lat){
+    let queryURLUVIndex = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+apiKey;
+    $.ajax({
+        url: queryURLUVIndex,
+        method: "GET"
+    }).then(
+        function (res) {
+            console.log(res);
+            indexUV = res.value;
+            console.log(indexUV);
+            renderUVIndexToday(indexUV)
+            return indexUV;                  
+        });
+}
+
+function getCurrentWeather(city) {
+    clear();
+    addCityToLS(city);
+    var queryWeatherUrl = queryURL + city + "&appid=" + apiKey;
+
     $.ajax({
         url: queryWeatherUrl,
         method: "GET"
     }).then(
-            function(res) {
+        function (res) {
             console.log(res);
-            var city = res.name;
-            var humidity = res.main.humidity
-
-            renderTodayWeather(city, humidity)
-
-            addCityToLS(city)
-
+            let city = res.name;
+            let humidity = res.main.humidity
+            let temperature = Math.round((res.main.temp - 273.15) * 100) / 100;
+            let wind = Math.round((res.wind.speed * 1.6) * 100) / 100;
+            let iconID = res.weather[0].icon;
+            let iconURL = `https://openweathermap.org/img/wn/${iconID}@2x.png`
+            console.log(city, humidity, temperature, wind)
+            renderTodayWeather(city, humidity, iconURL, temperature, wind)
+            let lon = res.coord.lon;
+            let lat = res.coord.lat;
+            getUVIndex(lon,lat);
         });
 
 }
 
-function getFiveDayForecast(city){
-     console.log(city);
+function getFiveDayForecast(city) {
+    console.log(city);
 
 }
 
-function renderTodayWeather(city, humidity){
+function renderTodayWeather(city, humidity, iconURL, temperature, wind) {
+    // $("#dateCurrent").text(`Date: ${city}`);
+    $("#cityCurrent").text(`City: ${city}`);
+    $("#tempCurrent").text(`Temperature: ${temperature} °C`);
+    $("#humidityCurrent").text(`City: ${humidity}`);
+    $("#windCurrent").text(`Wind speed: ${wind}`);
     
+    let img = $('<img>');
+    img.attr("src", iconURL);
+    $("#iconImage").append(img);
+
+
 }
 
-function addCityToLS(city){
+function renderUVIndexToday(indexUV){
+    console.log(indexUV);
+    $("#UVCurrent").text(`UV Index: ${indexUV}`);
+}
+
+function addCityToLS(city) {
 
 
     searchedCities.push(city)
-        console.log(searchedCities);
-    localStorage.setItem("cities", searchedCities);
+    console.log(searchedCities);
+    localStorage.setItem("cities", JSON.stringify(searchedCities));
 }
-
-function getCityFromLS(cities){
-
-    for (let i = 0; i < cities.length; i++) {
-
-        $(previousSearches[i]).val(localStorage.getItem(cities[i]));
-        consoleLog(previousSearches);
-        return(previousSearches);
-      };
-
-}
-
-// create function which makes initial query with last localstorage city
-
-// create search input and submit button in id search
-
-// create list of previous search options as read from localstorage
-// attach list to id previous
-
-// create function on click to start query
-
-// make first query always last entry in localstorage
-
-
-// fill first row with results
-// 1. city and date, 2. temperature in celsius, 
-// 3. humidity, 4. wind speed, 5.uv index
-
-// fill second row with results
-// 1.date, 2. weather symbol, 3. temp, 4. humidity
-// 5 columns
-
-
-
-
-
-
-
-
-// .error
-// create alert in case city is no found
-
-
-// .then 
-// store selected city in localstorage
-//
-
-
